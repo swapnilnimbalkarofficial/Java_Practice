e1e2424eadffb0e616347fd93103e44a0147b159
Swapnil Nimbalkar <swapnilnimbalkarofficial@gmail.com>
2025-01-30 22:22:12 +0530

    Add files via upload

---
+++ Package/Demo.java
@@ -0,0 +1,9 @@
+package a.b.c.d;
+
+public class Demo {
+    public int a, b;
+
+    public void display() {
+        System.out.println("display method of Demo");
+    }
+}
---
+++ Package/MainClass.java
@@ -0,0 +1,15 @@
+import a.b.c.d.Demo;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Demo d1 = new Demo();
+        d1.a = 10;
+        d1.b = 20;
+        d1.display();
+
+        Demo d2 = new Demo();
+        d2.a = 30;
+        d2.b = 40;
+        d2.display();
+    }
+}
